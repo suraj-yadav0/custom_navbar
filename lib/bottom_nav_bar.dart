1ec9fd7972493a7d62c95c87b51d7dafd796bbac
Suraj Yadav <90672206+suraj-yadav0@users.noreply.github.com>
2024-01-11 11:53:48 +0530

    second commit

--- lib/bottom_nav_bar.dart
+++ lib/bottom_nav_bar.dart
@@ -14,15 +14,37 @@ class BottomNavigationBarwithAnimation extends StatefulWidget {
 
 class _BottomNavigationBarwithAnimationState
     extends State<BottomNavigationBarwithAnimation> {
+  List<SMIBool> riveIconInputs = [];
+
+  List<StateMachineController?> controllers = [];
+
+  int selcectedIndex = 0;
+
+  List<String> pages = ["Chat","Search","History","Notification","Profile"];
+
+  void animateTheIcon(int index) {
+    riveIconInputs[index].change(true);
+    Future.delayed(const Duration(seconds: 1), () {
+      riveIconInputs[index].change(false);
+    });
+  }
+
+  @override
+  void dispose() {
+    for( var controller in controllers) {
+      controller?.dispose();
+    }
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: 
-       SafeArea(
-         child: Container(
-           
+      body: Center(child: Text(pages[selcectedIndex],style: const TextStyle(fontSize: 24,fontWeight: FontWeight.bold),),),
+      bottomNavigationBar: SafeArea(
+        child: Container(
           padding: const EdgeInsets.all(12),
-          margin: const EdgeInsets.symmetric(horizontal: 24,vertical: 24),
+          margin: const EdgeInsets.symmetric(horizontal: 24, vertical: 24),
           decoration: BoxDecoration(
               color: bottomNavBgColor.withOpacity(0.8),
               borderRadius: const BorderRadius.all(
@@ -30,27 +52,82 @@ class _BottomNavigationBarwithAnimationState
               ),
               boxShadow: [
                 BoxShadow(
-                    color: bottomNavBgColor.withOpacity(0.3),
-                    offset: const Offset(0, 20),
-                    blurRadius: 20,
-                    )
-             ]
-              ),
+                  color: bottomNavBgColor.withOpacity(0.3),
+                  offset: const Offset(0, 20),
+                  blurRadius: 20,
+                )
+              ]),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: List.generate(
-                bottomNavItems.length,
-                (index) => SizedBox(
+            children: List.generate(bottomNavItems.length, (index) {
+              final riveIcon = bottomNavItems[index].rive;
+              return GestureDetector(
+                onTap: () {
+                  animateTheIcon(index);
+                  setState(() {
+                    selcectedIndex = index;
+                  });
+                },
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    AnimatedBar(isActive: selcectedIndex == index),
+                    SizedBox(
                       height: 36,
                       width: 36,
-                      child: RiveAnimation.asset(
-                        bottomNavItems[index].rive.src,
-                        artboard: bottomNavItems[index].rive.artboard,
+                      child: Opacity(
+                        opacity: selcectedIndex == index ? 1 : .5,
+                        child: RiveAnimation.asset(
+                          riveIcon.src,
+                          artboard: riveIcon.artboard,
+                          onInit: (artboard) {
+                            StateMachineController? controller =
+                                StateMachineController.fromArtboard(
+                                    artboard, riveIcon.stateMachineName);
+                      
+                            artboard.addController(controller!);
+
+                            controllers.add(controller);
+                      
+                            controller.findInput<bool>('active');
+                      
+                            riveIconInputs.add(
+                                controller.findInput<bool>('active') as SMIBool);
+                          },
+                        ),
                       ),
-                    )),
+                    ),
+                  ],
+                ),
+              );
+            }),
           ),
-             ),
-       ),
+        ),
+      ),
+    );
+  }
+}
+
+class AnimatedBar extends StatelessWidget {
+  const AnimatedBar({
+    super.key,
+    required this.isActive,
+  });
+
+final bool isActive;
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 200),
+      margin: const EdgeInsets.only(bottom: 2),
+      height: 4,
+      width: isActive ?  20 : 0,
+      decoration: const BoxDecoration(
+        color: Color(0xFF81B4FF),
+        borderRadius: BorderRadius.all(
+          Radius.circular(12),
+        ),
+      ),
     );
   }
 }
